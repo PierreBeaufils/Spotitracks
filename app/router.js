2870f6d6395e5898f228b609c3e7069351a2c209
Pierre Beaufils <pierre.beaufils@outlook.fr>
2020-12-22 18:57:29 +0100

    responsive css

--- app/router.js
+++ app/router.js
@@ -4,15 +4,12 @@ const router = express.Router();
 const mainController = require('./mainController');
 
 
-
-
 router.get('/', mainController.home);
 router.get('/login', mainController.login);
 router.get('/callback', mainController.callback);
 router.get('/artists/top', mainController.topArtists);
 router.get('/tracks/top', mainController.topTracks);
 router.get('/logout', mainController.logout);
-
 
 
 module.exports = router;
